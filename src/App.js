881b346e98aed22b78b75482406318c0681d1744
Layne <layneingramtaylor@gmail.com>
2022-06-26 12:19:26 -0500

    initial commit; styling complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import star from './assets/icon-star.svg';
+import thankYouIcon from './assets/illustration-thank-you.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='card-wrapper'>
+        <div className='card thank-you--card'>
+          <img src={thankYouIcon} alt='' />
+          <button className='btn thank-you-btn'>You selected 4 out of 5</button>
+          <h1 className='card-title'>Thank you!</h1>
+          <p className='card-text'>
+            We appreciate you taking the time to give a rating. If you ever need
+            more support, don’t hesitate to get in touch!
+          </p>
+        </div>
+      </div>
+      {/* <div className='card-wrapper'>
+        <div className='card'>
+          <div className='star-wrapper'>
+            <img className='star' src={star} alt='' />
+          </div>
+          <h1 className='card-title'>How did we do?</h1>
+          <p className='card-text'>
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
+          </p>
+          <div className='rating-btn-wrapper'>
+            <button className='rating-btn'>1</button>
+            <button className='rating-btn'>2</button>
+            <button className='rating-btn'>3</button>
+            <button className='rating-btn'>4</button>
+            <button className='rating-btn'>5</button>
+          </div>
+          <button className='btn submit-btn'>submit</button>
+        </div>
+      </div> */}
+    </>
   );
 }
 
